4dce3a3468e848398abd6c97b977f1499f445f04
dmostafiz <dev.mostafiz@gmail.com>
2023-12-01 01:21:28 +0600

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,7 @@ export default function Home() {
     }
 
 
-    const validateEmail = (checkEmail) => {
+    function validateEmail(checkEmail){
       return checkEmail.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
